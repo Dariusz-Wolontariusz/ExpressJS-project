743cf138a6e8a91deb06afa2d36d993fe59f57aa
Dariusz-Wolontariusz <darek.ciazynski@protonmail.com>
2022-12-30 15:53:22 +0100

    new routes in markets

--- src/routes/markets.js
+++ src/routes/markets.js
@@ -2,20 +2,42 @@ import { Router } from 'express';
 
 const router = Router();
 
-const marketList = [
+const marketsList = [
     {
+        id: 1,
         shop: 'Kiwi',
+        kilometers: 0.6,
     },
     {
-        shop: 'Rema 1000'
+        id: 2,
+        shop: 'Rema1000',
+        kilometers: 2,
     },
     {
-        shop: 'Sunn mat'
+        id: 3,
+        shop: 'Meny',
+        kilometers: 3.2,
+    },
+    {
+        id: 4,
+        shop: 'Sunn mat',
+        kilometers: 2.6,
+    },
+    {
+        id: 5,
+        shop: 'Coop',
+        kilometers: 3.5,
+
     }
 ]
 
 router.get('/', (req, res) => {
-    res.send(marketList)
+    const { kilometers } = req.query;
+    const parsedKm = parseInt(kilometers);
+    if (!isNaN(parsedKm)) {
+    const filteredStores = marketsList.find(d => d.kilometers <= parsedKm)
+    }
+    res.send(marketsList)
 });
 
 export default router;
